42449ba663a23236351773bf98df7c71e262818c
Josh Justice <me@codingitwrong.com>
2022-11-15 16:44:32 -0500

    Set theme color to green

--- src/useCustomTheme.js
+++ src/useCustomTheme.js
@@ -1,8 +1,19 @@
 import {useColorScheme} from 'react-native';
 import {MD2DarkTheme, MD2LightTheme} from 'react-native-paper';
 
+const THEME_COLOR = '#4caf50';
+
 export default function useCustomTheme() {
   const colorScheme = useColorScheme() ?? 'light';
   const baseTheme = colorScheme === 'dark' ? MD2DarkTheme : MD2LightTheme;
-  return baseTheme;
+
+  const theme = {
+    ...baseTheme,
+    colors: {
+      ...baseTheme.colors,
+      primary: THEME_COLOR,
+    },
+  };
+
+  return theme;
 }
