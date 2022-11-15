9242d4ad9c001393df4d587847a2be29a814c228
Josh Justice <me@codingitwrong.com>
2022-11-15 16:43:27 -0500

    Style drawer background to dark

--- src/components/CustomNavigationDrawer.js
+++ src/components/CustomNavigationDrawer.js
@@ -1,13 +1,18 @@
 import {DrawerContentScrollView} from '@react-navigation/drawer';
-import {Drawer} from 'react-native-paper';
+import {Drawer, useTheme} from 'react-native-paper';
 
 export default function CustomNavigationDrawer(props) {
   const {state, navigation} = props;
+  const theme = useTheme();
+
+  const scrollViewStyle = {
+    backgroundColor: theme.colors.background,
+  };
 
   const isSelected = index => index === state.index;
 
   return (
-    <DrawerContentScrollView {...props}>
+    <DrawerContentScrollView style={scrollViewStyle} {...props}>
       {state.routes.map((route, index) => (
         <Drawer.Item
           key={route.key}
