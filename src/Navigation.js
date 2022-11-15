555e72161e1d0ce713b0945cb9ac39fbc3008214
Josh Justice <me@codingitwrong.com>
2022-11-15 07:53:20 -0500

    Set up initial navigation

---
+++ src/Navigation.js
@@ -0,0 +1,84 @@
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {Pressable, Text} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+function HomeRoot() {
+  const navigation = useNavigation();
+  return (
+    <>
+      <Text>HomeRoot</Text>
+      <Pressable onPress={() => navigation.navigate('HomeDetail')}>
+        <Text>Go to Detail</Text>
+      </Pressable>
+    </>
+  );
+}
+
+function HomeDetail() {
+  const navigation = useNavigation();
+  return (
+    <>
+      <Text>HomeDetail</Text>
+      <Pressable onPress={() => navigation.pop()}>
+        <Text>Back to HomeRoot</Text>
+      </Pressable>
+    </>
+  );
+}
+
+function OtherRoot() {
+  return <Text>OtherRoot</Text>;
+}
+
+const HomeStack = createNativeStackNavigator();
+function Home() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeRoot"
+        component={HomeRoot}
+        options={{title: 'Home'}}
+      />
+      <HomeStack.Screen
+        name="HomeDetail"
+        component={HomeDetail}
+        options={{title: 'Home Detail'}}
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+const OtherStack = createNativeStackNavigator();
+function Other() {
+  return (
+    <OtherStack.Navigator>
+      <OtherStack.Screen
+        name="OtherRoot"
+        component={OtherRoot}
+        options={{title: 'Other'}}
+      />
+    </OtherStack.Navigator>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+function NavigationContents() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Other" component={Other} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function Navigation() {
+  // IMPORTANT: NavigationContainer needs to not rerender too often or
+  // else Safari and Firefox error on too many history API calls. Put
+  // any hooks in NavigationContents so this parent doesn't rerender.
+  return (
+    <NavigationContainer>
+      <NavigationContents />
+    </NavigationContainer>
+  );
+}
