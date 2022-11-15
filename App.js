7c1586d4b9821bbf40bffb980c6afa4febb50f86
Josh Justice <me@codingitwrong.com>
2022-11-15 16:38:51 -0500

    Set up RN Paper

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import {StatusBar} from 'expo-status-bar';
 import Navigation from './src/Navigation';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 export default function App() {
   return (
-    <>
+    <PaperProvider>
       <StatusBar style="auto" />
       <Navigation />
-    </>
+    </PaperProvider>
   );
 }
